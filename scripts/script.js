031fb83b164417dc350adf145918f2e62e7a1b9f
Rojen Chakradhar <chakradhar@rojen.me>
2026-01-03 00:00:28 +0545

    profile page completed

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,28 @@ let loader = document.querySelector("#loader");
 setTimeout(() => {
 	loader.style.top = "-100dvh";
 }, 1000);
+
+function edit() {
+	let editPage =  document.querySelector("#profile-edit");
+	let showPage =  document.querySelector("#profile-show");
+	editPage.style.display = "flex";
+	showPage.style.display = "none";
+}
+
+function back() {
+	let editPage =  document.querySelector("#profile-edit");
+	let showPage =  document.querySelector("#profile-show");
+	editPage.style.display = "none";
+	showPage.style.display = "flex";
+}
+
+function save() {
+	let toast = document.querySelector("#toast");
+	back();
+	setTimeout(() => {
+		toast.style.right = "10px";
+	}, 500);
+	setTimeout(() => {
+		toast.style.right = "-500px";
+	}, 3000);
+}
